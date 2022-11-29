bbceeb532f28b9ca4a220fb9e819532bfcf4314e
tomohiro <tomohirofarm@gmail.com>
2022-11-29 12:29:16 +0900

    set数を入れてみた

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import "./Timer.css"
 
 function Timer() {
-  const [time, setTime] = useState(10);
+  const [time, setTime] = useState(20);
+  const [isSet, setIsSet] = useState(0);
 
 
   let timerId;
@@ -14,17 +15,20 @@ function Timer() {
 
      if(time === 0) {
       clearInterval(timerId);
+      setIsSet((set) => set + 1 )
      }
+   }
+
+   const hocount = () => {
+    timerId = setInterval(() => {
+      setTime((prevtime) => prevtime - 1)
+     }, 1000)
 
    }
 
   useEffect(() => {
-  
      count();
-       
-
     return () => {  
-
       clearInterval(timerId);
     }
   },[time])
@@ -37,6 +41,7 @@ function Timer() {
   return (
     <>
     <div className='time'>残り{time}秒</div>
+    <p className='set'> set : {isSet}</p>
     <button className='start' onClick={() => count()}>start</button>
     <button className='stop' onClick={() => handleStop()}>stop</button>
     </>
