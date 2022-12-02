3a29e4123acc648f55cebdcd146b5eb04087a7eb
tomohiro <tomohirofarm@gmail.com>
2022-12-02 11:36:38 +0900

    かなり進歩した

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import "./Timer.css"
 
 function Timer() {
-  const [time, setTime] = useState(20);
+  const [time, setTime] = useState(5);
   const [isSet, setIsSet] = useState(0);
+  const [issetTime, setIssetTime ] = useState(10)
 
 
   let timerId;
+  let tomotimerId;
  
    const count = () => {
      timerId = setInterval(() => {
@@ -15,32 +17,38 @@ function Timer() {
 
      if(time === 0) {
       clearInterval(timerId);
+      hocount();
+    }
+  }
+
+  const hocount = () => {
+    tomotimerId = setInterval(() => {
+      setIssetTime((tomo) => tomo - 1)
+    }, 1000)
+    
+    if(issetTime === 0) {
+      clearInterval(tomotimerId);
       setIsSet((set) => set + 1 )
      }
    }
 
-   const hocount = () => {
-    timerId = setInterval(() => {
-      setTime((prevtime) => prevtime - 1)
-     }, 1000)
-
-   }
-
   useEffect(() => {
      count();
-    return () => {  
+    return () => {    
       clearInterval(timerId);
+      clearInterval(tomotimerId);
     }
-  },[time])
+  },[time, issetTime])
   
    const handleStop = () => {
-    clearInterval(timerId)
+    clearInterval(timerId);
  
    }
 
   return (
     <>
     <div className='time'>残り{time}秒</div>
+    <div className='time'>残り{issetTime}秒</div>
     <p className='set'> set : {isSet}</p>
     <button className='start' onClick={() => count()}>start</button>
     <button className='stop' onClick={() => handleStop()}>stop</button>
